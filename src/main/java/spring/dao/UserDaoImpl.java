6378910fba7dbcb51a684b15274b08869fd3df38
Andrii Bardin <andrii.bardin@gmail.com>
2021-02-08 17:10:30 +0200

    spring 1 homework

---
+++ src/main/java/spring/dao/UserDaoImpl.java
@@ -0,0 +1,29 @@
+package spring.dao;
+
+import java.util.List;
+import javax.persistence.TypedQuery;
+import org.hibernate.SessionFactory;
+import org.springframework.stereotype.Repository;
+import spring.model.User;
+
+@Repository
+public class UserDaoImpl implements UserDao {
+
+    private final SessionFactory sessionFactory;
+
+    public UserDaoImpl(SessionFactory sessionFactory) {
+        this.sessionFactory = sessionFactory;
+    }
+
+    @Override
+    public void add(User user) {
+        sessionFactory.openSession().save(user);
+    }
+
+    @Override
+    public List<User> listUsers() {
+        TypedQuery<User> query = sessionFactory.openSession()
+                .createQuery("from User", User.class);
+        return query.getResultList();
+    }
+}
