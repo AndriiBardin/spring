486b66177ff9e2908f885ca621bff70e49f2d0d4
Andrii Bardin <andrii.bardin@gmail.com>
2021-02-08 17:20:58 +0200

    spring 1 homework cleaned #2

--- src/main/java/spring/dao/UserDaoImpl.java
+++ src/main/java/spring/dao/UserDaoImpl.java
@@ -1,8 +1,9 @@
 package spring.dao;
 
 import java.util.List;
-import javax.persistence.TypedQuery;
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
 import org.springframework.stereotype.Repository;
 import spring.model.User;
 
@@ -17,13 +18,31 @@ public class UserDaoImpl implements UserDao {
 
     @Override
     public void add(User user) {
-        sessionFactory.openSession().save(user);
+        Transaction transaction = null;
+        Session session = null;
+        try {
+            session = sessionFactory.openSession();
+            transaction = session.beginTransaction();
+            session.save(user);
+            transaction.commit();
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            throw new RuntimeException("Can't insert user entity" + user, e);
+        } finally {
+            if (session != null) {
+                session.close();
+            }
+        }
     }
 
     @Override
     public List<User> listUsers() {
-        TypedQuery<User> query = sessionFactory.openSession()
-                .createQuery("from User", User.class);
-        return query.getResultList();
+        try (Session session = sessionFactory.openSession()) {
+            return session.createQuery("select u from User u", User.class).getResultList();
+        } catch (Exception e) {
+            throw new RuntimeException("Can't list all users", e);
+        }
     }
 }
